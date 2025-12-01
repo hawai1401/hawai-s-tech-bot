3d61db33396857015388bd60b9851192ef257d55
hawai1401 <hawai1401@ik.me>
2025-12-01 13:40:16 +0100

    Ajout du module de logs des invitations

--- src/events/logs/invitation/inviteCreate.ts
+++ src/events/logs/invitation/inviteCreate.ts
@@ -1,6 +1,46 @@
-import type { Invite } from "discord.js";
+import { EmbedBuilder, type Invite, type TextChannel } from "discord.js";
 import type { botClient } from "../../../index.js";
+import config from "../../../../config.json" with { type: "json" };
 
 export const type = "inviteCreate";
 
-export const event = async (client: botClient, invite: Invite) => {};
+export const event = async (client: botClient, invite: Invite) => {
+  if (!invite.inviterId) return;
+  const inviteur = await client.users.fetch(invite.inviterId);
+  const embed = new EmbedBuilder()
+    .setColor(config.embed.success)
+    .setThumbnail(inviteur.displayAvatarURL())
+    .addFields({
+      name: "🔗 - Invitation",
+      value: `>>> **Salon** : ${invite.channel} \`${invite.channel!.name}\`
+         **Nombre maximum d'utilisation** : ${
+           invite.maxUses && invite.maxUses === 0
+             ? "♾️"
+             : invite.maxUses
+             ? invite.maxUses
+             : "Inconnu"
+         }\n**Lien** : ${invite.url}\n**Expire** ${
+        invite.expiresTimestamp
+          ? `<t:${Math.floor(
+              invite.expiresTimestamp / 1000
+            )}:R> (<t:${Math.floor(invite.expiresTimestamp / 1000)}:F>)`
+          : "Jamais"
+      }\n**Temporaire** : ${
+        invite.temporary ? config.emojis.success : config.emojis.error
+      }`,
+    })
+    .addFields({
+      name: "👤 - Inviteur",
+      value: `>>> **ID** : ${invite.inviterId}\n**Pseudo** : ${inviteur.username}`,
+    })
+    .setFooter({
+      text: "Invitation créée",
+      iconURL: invite.guild?.iconURL() ?? "",
+    })
+    .setTimestamp();
+
+  const channel = (await client.channels.fetch(
+    "1445024679856181323"
+  )) as TextChannel;
+  channel.send({ embeds: [embed] });
+};
--- src/events/logs/invitation/inviteDelete.ts
+++ src/events/logs/invitation/inviteDelete.ts
@@ -1,6 +1,62 @@
-import type { Invite } from "discord.js";
+import {
+  AuditLogEvent,
+  EmbedBuilder,
+  Guild,
+  type Invite,
+  type TextChannel,
+} from "discord.js";
 import type { botClient } from "../../../index.js";
+import config from "../../../../config.json" with { type: "json" };
 
 export const type = "inviteDelete";
 
-export const event = async (client: botClient, invite: Invite) => {};
+export const event = async (client: botClient, invite: Invite) => {
+    if (!invite.guild || !(invite.guild instanceof Guild))
+    return;
+  const log = (
+    await invite.guild.fetchAuditLogs({
+      type: AuditLogEvent.InviteDelete,
+      limit: 1,
+    })
+  ).entries.first();
+  const admin = log?.executorId
+    ? await client.users.fetch(log.executorId)
+    : null;
+  const embed = new EmbedBuilder()
+    .setColor(config.embed.error)
+    .addFields({
+      name: "🔗 - Invitation",
+      value: `>>> **Salon** : ${invite.channel} \`${invite.channel!.name}\`
+         **Nombre maximum d'utilisation** : ${
+           invite.maxUses && invite.maxUses === 0
+             ? "♾️"
+             : invite.maxUses
+             ? invite.maxUses
+             : "Inconnu"
+         }\n**Lien** : ${invite.url}\n**Expire** ${
+        invite.expiresTimestamp
+          ? `<t:${Math.floor(
+              invite.expiresTimestamp / 1000
+            )}:R> (<t:${Math.floor(invite.expiresTimestamp / 1000)}:F>)`
+          : "Jamais"
+      }\n**Temporaire** : ${
+        invite.temporary ? config.emojis.success : config.emojis.error
+      }`,
+    })
+    .setFooter({
+      text: "Invitation supprimée",
+      iconURL: invite.guild?.iconURL() ?? "",
+    })
+    .setTimestamp();
+
+  if (admin)
+    embed.addFields({
+      name: "🛡️ - Administrateur",
+      value: `>>> **ID** : ${admin.id}\n**Pseudo** : ${admin} \`${admin.username}\``,
+    });
+
+  const channel = (await client.channels.fetch(
+    "1445024679856181323"
+  )) as TextChannel;
+  channel.send({ embeds: [embed] });
+};
