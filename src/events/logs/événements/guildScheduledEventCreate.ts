c17d1414213a525478cae9ec1b87a9000e0fe8b9
hawai1401 <hawai1401@ik.me>
2025-12-02 22:39:31 +0100

    Ajout du module de log des événements

--- src/events/logs/événements/guildScheduledEventCreate.ts
+++ src/events/logs/événements/guildScheduledEventCreate.ts
@@ -1,9 +1,73 @@
-import type { GuildScheduledEvent } from "discord.js";
+import {
+  EmbedBuilder,
+  GuildScheduledEventEntityType,
+  TextChannel,
+  type GuildScheduledEvent,
+} from "discord.js";
 import type { botClient } from "../../../index.js";
+import config from "../../../../config.json" with { type: "json" };
 
 export const type = "guildScheduledEventCreate";
 
 export const event = async (
   client: botClient,
   guildScheduledEvent: GuildScheduledEvent
-) => {};
+) => {
+  const embed = new EmbedBuilder()
+    .setColor(config.embed.success)
+    .setAuthor({
+      name: guildScheduledEvent.creator?.username ?? "",
+      iconURL: guildScheduledEvent.creator?.displayAvatarURL() ?? "",
+    })
+    .addFields({
+      name: "ℹ️ - Informations",
+      value: `>>> **ID** : ${guildScheduledEvent.id}\n**Nom** : ${
+        guildScheduledEvent.name
+      }\n**Description** : _${
+        guildScheduledEvent.description?.slice(0, 500) ?? "Aucune"
+      }_${
+        guildScheduledEvent.description &&
+        guildScheduledEvent.description.length > 500
+          ? "..."
+          : ""
+      }\n\n**Lieu** : ${
+        guildScheduledEvent.entityType ===
+        GuildScheduledEventEntityType.External
+          ? guildScheduledEvent.entityMetadata!.location
+          : `${guildScheduledEvent.channel} \`${
+              guildScheduledEvent.channel!.name
+            }\``
+      }\n**Date de création** : <t:${Math.floor(
+        guildScheduledEvent.createdTimestamp / 1000
+      )}:R> (<t:${Math.floor(
+        guildScheduledEvent.createdTimestamp / 1000
+      )}:F>)\n**Date de commencement** : <t:${Math.floor(
+        guildScheduledEvent.scheduledStartTimestamp! / 1000
+      )}:R> (<t:${Math.floor(
+        guildScheduledEvent.scheduledStartTimestamp! / 1000
+      )}:F>)\n**Date de fin** : ${
+        guildScheduledEvent.scheduledEndTimestamp
+          ? `<t:${Math.floor(
+              guildScheduledEvent.scheduledEndTimestamp / 1000
+            )}:R> (<t:${Math.floor(
+              guildScheduledEvent.scheduledEndTimestamp / 1000
+            )}:F>)`
+          : "_Non saisie_"
+      }\n\n**Lien** : ${guildScheduledEvent.url}\n**Récurrent** : ${
+        guildScheduledEvent.recurrenceRule
+          ? config.emojis.success
+          : config.emojis.error
+      }`,
+    })
+    .setImage(guildScheduledEvent.coverImageURL({ size: 4096 }))
+    .setFooter({
+      text: "Événement créé",
+      iconURL: guildScheduledEvent.guild?.iconURL() ?? "",
+    })
+    .setTimestamp();
+
+  const channel = (await client.channels.fetch(
+    "1445513101029146745"
+  )) as TextChannel;
+  channel.send({ embeds: [embed] });
+};
--- src/events/logs/événements/guildScheduledEventUpdate.ts
+++ src/events/logs/événements/guildScheduledEventUpdate.ts
@@ -1,8 +1,11 @@
-import type {
-  GuildScheduledEvent,
-  PartialGuildScheduledEvent,
+import {
+  EmbedBuilder,
+  TextChannel,
+  type GuildScheduledEvent,
+  type PartialGuildScheduledEvent,
 } from "discord.js";
 import type { botClient } from "../../../index.js";
+import config from "../../../../config.json" with { type: "json" };
 
 export const type = "guildScheduledEventUpdate";
 
@@ -13,4 +16,141 @@ export const event = async (
     | PartialGuildScheduledEvent
     | null,
   newGuildScheduledEvent: GuildScheduledEvent
-) => {};
+) => {
+  if (!oldGuildScheduledEvent) return;
+
+  const embed = new EmbedBuilder()
+    .setColor(config.embed.warn)
+    .setAuthor({
+      name: newGuildScheduledEvent.creator?.username ?? "",
+      iconURL: newGuildScheduledEvent.creator?.displayAvatarURL() ?? "",
+    })
+    .setFooter({
+      text: "Événement modifié",
+      iconURL: newGuildScheduledEvent.guild?.iconURL() ?? "",
+    })
+    .setTimestamp();
+
+  const updated = [];
+
+  if (oldGuildScheduledEvent.name !== newGuildScheduledEvent.name) {
+    embed.addFields({
+      name: "ℹ️ - Informations",
+      value: `>>> **ID** : ${newGuildScheduledEvent.id}\n**Nom** : ${
+        oldGuildScheduledEvent.name
+      } ${config.emojis.arrow_right} ${
+        newGuildScheduledEvent.name
+      }\n\n**Date de création** : <t:${Math.floor(
+        newGuildScheduledEvent.createdTimestamp / 1000
+      )}:R> (<t:${Math.floor(
+        newGuildScheduledEvent.createdTimestamp / 1000
+      )}:F>)`,
+    });
+  } else {
+    embed.addFields({
+      name: "ℹ️ - Informations",
+      value: `>>> **ID** : ${newGuildScheduledEvent.id}\n**Nom** : ${
+        newGuildScheduledEvent.name
+      }\n\n**Date de création** : <t:${Math.floor(
+        newGuildScheduledEvent.createdTimestamp / 1000
+      )}:R> (<t:${Math.floor(
+        newGuildScheduledEvent.createdTimestamp / 1000
+      )}:F>)`,
+    });
+  }
+
+  if (oldGuildScheduledEvent.description !== newGuildScheduledEvent.description)
+    embed
+      .addFields({
+        name: "📤 - Ancienne description",
+        value: ">>> " + (oldGuildScheduledEvent.description ?? "Aucune"),
+      })
+      .addFields({
+        name: "📥 - Nouvelle description",
+        value: ">>> " + (newGuildScheduledEvent.description ?? "Aucune"),
+      });
+
+  if (
+    oldGuildScheduledEvent.scheduledStartAt !==
+    newGuildScheduledEvent.scheduledStartAt
+  )
+    updated.push(
+      `**Date de commencement** : <t:${Math.floor(
+        oldGuildScheduledEvent.scheduledStartTimestamp! / 1000
+      )}:R> (<t:${Math.floor(
+        oldGuildScheduledEvent.scheduledStartTimestamp! / 1000
+      )}:F>) ${config.emojis.arrow_right} <t:${Math.floor(
+        newGuildScheduledEvent.scheduledStartTimestamp! / 1000
+      )}:R> (<t:${Math.floor(
+        newGuildScheduledEvent.scheduledStartTimestamp! / 1000
+      )}:F>)`
+    );
+
+  if (
+    oldGuildScheduledEvent.scheduledEndTimestamp !==
+    newGuildScheduledEvent.scheduledEndTimestamp
+  )
+    updated.push(
+      `**Date de fin** : ${
+        oldGuildScheduledEvent.scheduledEndTimestamp !== 0
+          ? `<t:${Math.floor(
+              oldGuildScheduledEvent.scheduledEndTimestamp! / 1000
+            )}:R> (<t:${Math.floor(
+              oldGuildScheduledEvent.scheduledEndTimestamp! / 1000
+            )}:F>)`
+          : "Aucune"
+      } ${config.emojis.arrow_right} ${
+        newGuildScheduledEvent.scheduledEndTimestamp !== 0
+          ? `<t:${Math.floor(
+              newGuildScheduledEvent.scheduledEndTimestamp! / 1000
+            )}:R> (<t:${Math.floor(
+              newGuildScheduledEvent.scheduledEndTimestamp! / 1000
+            )}:F>)`
+          : "Aucune"
+      }`
+    );
+
+  if (oldGuildScheduledEvent.channel !== newGuildScheduledEvent.channel)
+    updated.push(
+      `**Salon** : ${oldGuildScheduledEvent.channel ?? "Aucun"} \`${
+        oldGuildScheduledEvent.channel?.name ?? "Aucun"
+      }\` ${config.emojis.arrow_right} ${
+        newGuildScheduledEvent.channel ?? "Aucun"
+      } \`${newGuildScheduledEvent.channel?.name ?? "Aucun"}\``
+    );
+
+  if (
+    oldGuildScheduledEvent.entityMetadata?.location !==
+    newGuildScheduledEvent.entityMetadata?.location
+  )
+    updated.push(
+      `**Lieu** : ${
+        oldGuildScheduledEvent.entityMetadata?.location ?? "Aucun"
+      } ${config.emojis.arrow_right} ${
+        newGuildScheduledEvent.entityMetadata?.location ?? "Aucun"
+      }`
+    );
+
+  if (
+    oldGuildScheduledEvent.coverImageURL() !==
+      newGuildScheduledEvent.coverImageURL() &&
+    newGuildScheduledEvent.coverImageURL()
+  ) {
+    updated.push(`**Image** : Modifiée`);
+    embed.setImage(newGuildScheduledEvent.coverImageURL({ size: 4096 }));
+  }
+
+  if (updated.length === 0) return;
+
+  embed.addFields({
+    name: "✏️ - Informations modifiées",
+    value: ">>> " + updated.join("\n"),
+  });
+
+  // - Status
+
+  const channel = (await client.channels.fetch(
+    "1445513101029146745"
+  )) as TextChannel;
+  channel.send({ embeds: [embed] });
+};
